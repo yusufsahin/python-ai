f97bfe419ddd306099d1d228df704d21196ccb90
Yusuf Sahin <yusuf@yusufsahin.net>
2025-04-30 21:01:46 +0300

    empleoyee console app has been impl

--- Ch14ConsAppSQL/app.py
+++ Ch14ConsAppSQL/app.py
@@ -1,4 +1,4 @@
-from employee import add_employee
+from employee import add_employee, list_employees, update_employee, delete_employee, get_employee_by_id
 
 
 def main():
@@ -8,7 +8,8 @@ def main():
         print("2. List Employees")
         print("3. Update Employee")
         print("4. Delete Employee")
-        print("5. Exit")
+        print("5. Get Employee By Id")
+        print("6. Exit")
 
         choice = input("Select an option: ")
         if choice == "1":
@@ -22,14 +23,33 @@ def main():
                 print("Invalid salary")
                 continue
             add_employee(name, department, salary)
+            print("Employee added successfully")
         elif choice == "2":
             print("List Employees")
+            list_employees()
         elif choice == "3":
             print("Update Employee")
+            emp_id = input("Enter employee id: ")
+            name = input("Enter employee name: ")
+            department = input("Enter employee department: ")
+            salary = float(input("Enter employee salary: "))
+            print("Updating Employee...")
+            update_employee(emp_id,name, department, salary)
+            print("Employee updated successfully")
         elif choice == "4":
-            print("Delete Employee")
+            emp_id = int(input("Enter employee id: "))
+            print("Deleting Employee...")
+            delete_employee(emp_id)
+            print("Employee Deleted!")
         elif choice == "5":
-            print("Exit")
+            emp_id = int(input("Enter employee id: "))
+            employee = get_employee_by_id(emp_id)
+            if employee is not None:
+                print(f"ID : {employee[0]}, Name : {employee[1]},Dept:{employee[2]}, Salary : {employee[3]}")
+            else:
+                print("Employee not found")
+        elif choice=="6":
+            print("Good Bye...")
             break
         else:
             print("Invalid input. Please select 1-5.")
--- Ch14ConsAppSQL/employee.py
+++ Ch14ConsAppSQL/employee.py
@@ -14,3 +14,34 @@ def add_employee(name,department,salary):
         conn.rollback()
     finally:
         release_connection(conn)
+def list_employees():
+    with  get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id,name,department,salary FROM employees")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+def update_employee(p_emp_id,p_name, p_department, p_salary):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE employees SET name=%s,department=%s,salary=%s WHERE id=%s",
+                (p_name,p_department,p_salary,p_emp_id)
+            )
+            conn.commit()
+
+def delete_employee(emp_id):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM employees WHERE id=%s",(emp_id,))
+            conn.commit()
+def get_employee_by_id(emp_id):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT id, name, department, salary FROM employees WHERE id=%s",
+                (emp_id,)
+            )
+            row = cur.fetchone()
+            return row
