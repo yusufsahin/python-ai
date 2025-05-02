e0919f86ce048732db6b98504060f8a8c17f05f2
Yusuf Sahin <yusuf@yusufsahin.net>
2025-05-02 21:11:04 +0300

    List employee fast api update

--- Ch14FastApiEmp/routes.py
+++ Ch14FastApiEmp/routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 from crud import add_employee, list_employees
 from models import EmployeeCreate, EmployeeOut
-
+from typing import List
 router = APIRouter()
 
 @router.post("/employee", response_model=int)
 async def add_employee_route(employee: EmployeeCreate):
     return await add_employee(employee)
 
-@router.get("/employee", response_model=list[EmployeeOut])
+@router.get("/employee", response_model=List[EmployeeOut])
 async def get_employee_route():
     return  await list_employees()
